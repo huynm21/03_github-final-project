125425ff9f0ed64ae7c40d1abb5571a711b14758
huynm21 <140838251+huynm21@users.noreply.github.com>
2023-07-30 23:43:29 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <huynm21>
+   #  huynm21
    # Input:
    # p, principal amount
    # t, time period in years
